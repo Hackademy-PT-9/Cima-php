c2f829ae2b1ef33eba4f4eff180fb519c8db71eb
Marco Cima <marcoskrome3@gmail.com>
2023-09-02 17:04:37 +0200

    salando

---
+++ progettoSalando/Salando/resources/views/template/main.blade.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Modern Business - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="scss/styles.scss" rel="stylesheet" />
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <x-navbar/>
+    <body class="d-flex flex-column h-100">
+        <main class="flex-shrink-0">
+             {{$slot}}
+        </main>
+        <!-- Footer-->
+        <x-footer/>
+    </body>
+    
+</html>
---
+++ progettoSalando/Salando/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\PublicController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/',[PublicController::class, 'homepage'])->name('homepage');
+
+Route::get('/about', [PublicController::class, 'about'])->name('about');
+
+Route::get('/contact', [PublicController::class, 'contact'])->name('contatti');
+
+Route::get('/pricing', [PublicController::class, 'pricing'])->name('prezzi');
+
+Route::get('/faq',[PublicController::class, 'faq'])->name('domande');
+
+Route::get('/prodotto/{dettagli}',[CompanyController::class, 'dettagli'])->name('dettagliprodotto');
